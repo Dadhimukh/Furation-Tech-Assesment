b4ec4972151b1f2f93c67459516d35dc7e6ed083
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-06-20 06:55:17 +0530

    creating route setup

--- routes/home.js
+++ routes/home.js
@@ -12,20 +12,14 @@ router.post("/add", (req, res, next) => {
   const bookName = req.body.bookName;
   const bookPrice = req.body.bookPrice;
 
-  console.log(bookId, bookName, bookPrice);
+  // console.log(bookId, bookName, bookPrice);
 
-  const books = new book({
+  const books = book.create({
     bookId,
     bookName,
     bookPrice,
-  });
+  })
 
-  books.save((err) => {
-    if (err) {
-      console.log("Something went wrong to save data in database");
-    } else {
-      console.log("Data saved in Database Successfully");
-    }
-  });
+
 });
 module.exports = router;
